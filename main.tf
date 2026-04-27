9780e9bb181ca5c2f8eed60a44b3ce57bf13e55e
vinayapch <vinayadaniel.nms@gmail.com>
2026-04-27 23:31:36 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,3 +19,8 @@ resource "newrelic_alert_policy" "tf_policy" {
 resource "grafana_folder" "tf_folder" {
   title = "Managed by Terraform from vin"
 }
+provider "newrelic" {
+  account_id = "2198319"
+  api_key    = "NRAK-QPENLTIS45FRIQK98YTIKRD3T4N" # Use your full key here
+  region     = "EU"               # Try adding this line
+}
