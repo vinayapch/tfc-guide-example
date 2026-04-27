f36b133572726274d9c25fffd7f284d4e5ff2062
vinayapch <vinayadaniel.nms@gmail.com>
2026-04-27 23:36:30 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,5 +22,5 @@ resource "grafana_folder" "tf_folder" {
 provider "newrelic" {
   account_id = "2198319"
   api_key    = "NRAK-QPENLTIS45FRIQK98YTIKRD3T4N" # Use your full key here
-  region     = "EU"               # Try adding this line
+  region     = "US"               # Try adding this line
 }
